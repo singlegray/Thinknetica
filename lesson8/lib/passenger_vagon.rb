77219b9b78e3b5bb2b61e7211355f2c982f47607
singlegray <singlegray@gmail.com>
2020-04-25 12:06:23 +0300

    Create passenger_vagon.rb

---
+++ lesson8/lib/passenger_vagon.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative 'vagon'
+
+class PassengerVagon < Vagon
+  attr_reader :count_place, :uses_place, :type, :free_place, :occ_place
+
+  def initialize(number, type = ':pass', count_place = 0)
+    @number      = number
+    @count_place = count_place
+    @type        = type
+    @free_place  = count_place
+    @occ_place   = 0
+  end
+
+  def take_place
+    @occ_place += 1 if free_place.positive?
+    @free_place -= 1 if free_place.positive?
+  end
+end
