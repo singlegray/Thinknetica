d09c7e9716da43ff751a0cd46177befbfd5a72ca
singlegray <singlegray@gmail.com>
2020-04-25 12:05:44 +0300

    Create cargo_vagon.rb

---
+++ lesson7/lib/cargo_vagon.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require_relative 'vagon'
+
+class CargoVagon < Vagon
+  attr_reader :count_volume, :uses_place, :type, :free_volume, :occ_volume
+  
+  def initialize(number, type = ':cargo', count_volume = 0)
+    @number = number
+    @count_volume = count_place
+    @type = type
+    @free_volume  = count_place
+    @occ_volume   = 0
+  end
+
+  def take_place
+    @occ_volume += 1 if free_volume.positive?
+    @free_volume -= 1 if free_volume.positive?
+  end
+end
