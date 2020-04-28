1dd28d8f6e20db9d8d280ed15d49deee92be37ee
singlegray <singlegray@gmail.com>
2020-04-28 12:35:27 +0300

    Update station.rb

--- lesson9/lib/station.rb
+++ lesson9/lib/station.rb
@@ -11,6 +11,8 @@ class Station
   @count = 0
   STATION_NAME = /^[a-z]{3,}$/i.freeze
 
+  validate :name, :presence
+
   def self.all
     @@count
   end
@@ -37,20 +39,5 @@ class Station
 
   def departure(train)
     @trains.delete(train)
-  end
-
-  def validate!
-    validate_name_empty!
-    validate_station_name!
-  end
-
-  def validate_name_empty!
-    raise ArgumentError, "Name can't be empty" if @name.empty?
-  end
-
-  def validate_station_name!
-    if @name !~ STATION_NAME
-      raise ArgumentError, "Name '#{@name}' has no valid format"
-    end
   end
 end
