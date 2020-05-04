cceabd23877657299d2387f2d73df3d07a1c0759
singlegray <singlegray@gmail.com>
2020-05-04 09:54:21 +0300

    Create player.rb

---
+++ blackjack/lib/player.rb
@@ -0,0 +1,15 @@
+class Player
+  attr_accessor :hand, :coin, :score, :name
+
+  def initialize(name)
+    @hand  = []
+    @coin  = 100
+    @score = 0
+    @name  = name
+  end
+
+  def take_card
+
+  end
+
+end
