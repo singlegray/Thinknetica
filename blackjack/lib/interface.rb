3839476688f5664c3cfbfd6a3720be5c269f7b82
singlegray <singlegray@gmail.com>
2020-05-04 09:54:15 +0300

    Create interface.rb

---
+++ blackjack/lib/interface.rb
@@ -0,0 +1,16 @@
+class Interface
+  def initialize
+
+  end
+
+  def main_menu
+    puts '1 - New game'
+    puts '2 - Exit'
+  end
+
+  def menu
+    puts ''
+    puts '1 - Add'
+    puts '2 - Show'
+  end
+end
