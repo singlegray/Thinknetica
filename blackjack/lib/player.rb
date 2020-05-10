3e8ecfb961141475da41fd7de7df614c5eb21408
singlegray <singlegray@gmail.com>
2020-05-10 11:30:06 +0300

    Update player.rb

--- blackjack/lib/player.rb
+++ blackjack/lib/player.rb
@@ -1,15 +1,12 @@
+# frozen_string_literal: true
+
 class Player
   attr_accessor :hand, :coin, :score, :name
 
   def initialize(name)
-    @hand  = []
-    @coin  = 100
+    @hand = []
+    @coin = 100
     @score = 0
-    @name  = name
+    @name = name
   end
-
-  def take_card
-
-  end
-
 end
