70114668f26d0f2e185d136590b1bc0b7872ee4a
singlegray <singlegray@gmail.com>
2020-05-10 11:30:00 +0300

    Update interface.rb

--- blackjack/lib/interface.rb
+++ blackjack/lib/interface.rb
@@ -1,6 +1,10 @@
-class Interface
-  def initialize
+# frozen_string_literal: true
 
+class Interface
+  require_relative 'main'
+
+  def initialize
+    @game = Main.new
   end
 
   def main_menu
@@ -21,12 +25,37 @@ class Interface
 
   def show_hand(player)
     puts "#{player.name} your card"
-    player.hand.each { |i| print "#{i} " }
+    player.hand.each { |i| print "#{i.disp} " }
   end
 
   def get_name
     puts "Enter your name"
-    name = gets.chomp
-    return name
+    gets.chomp
+  end
+
+  # game
+  def start
+    @game.set_computer
+    @game.set_user(get_name)
+    clear_terminal
+    @game.deck.shuffle
+    @game.deals_card
+    @game.make_bet
+    show_hand(@game.player)
+    loop do
+      menu
+      case gets.to_i
+      when 1
+        clear_terminal
+        @game.player_take_card(@game.player, 1)
+        show_hand(@game.player)
+        @game.make_move_comp
+      when 2
+        clear_terminal
+        @game.open_card
+      when 3
+        @game.make_move_comp
+      end
+    end
   end
 end
