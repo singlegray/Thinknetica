7f00e3fd0e94e998ce27ac19acb84831f826a088
singlegray <singlegray@gmail.com>
2020-05-11 16:55:38 +0300

    Update player.rb

--- blackjack/lib/player.rb
+++ blackjack/lib/player.rb
@@ -9,4 +9,13 @@ class Player
     @score = 0
     @name = name
   end
+
+  def player_take_card(deck, count = 1)
+    return if hand.size >= 3
+    count.times do
+      card = deck.cards.pop
+      @hand.push(card)
+      @score += card.score
+    end
+  end
 end
